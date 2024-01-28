006927e8401139775591151a1893daf1f593b023
Ahmed <ahmedrajibbb@gmail.com>
2024-01-28 13:52:41 +0600

    Initial Commit

---
+++ ScrptHandler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrptHandler
+//
+//  Created by ahmed rajib on 28/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
